0499e19471172be99b5ba4afaa6d716b4f3e70b0
Wisn <wuyishun_kmk@outlook.com>
2018-01-06 14:12:06 +0800

    test channel

--- ReachTask/mainmodule/src/main/java/com/wisn/mainmodule/MainActivity.java
+++ ReachTask/mainmodule/src/main/java/com/wisn/mainmodule/MainActivity.java
@@ -2,6 +2,7 @@ package com.wisn.mainmodule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        startActivity(new Intent(this,TestRNActivity.class));
+        TextView textView=findViewById(R.id.text);
+        textView.setText(getResources().getString(R.string.host)+"\n packageName"+this.getApplication().getPackageName());
 
     }
 }
